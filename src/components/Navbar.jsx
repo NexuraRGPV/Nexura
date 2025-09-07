a5f96f5972dd0010b23deb4d7f1a03a0fbca514c
omchaturvedi0412 <omchaturvedi201@gmail.com>
2025-09-08 02:32:43 +0530

    Uploaded React project with navbar scroll fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
       {/* Logo */}
@@ -9,8 +15,15 @@ const Navbar = () => {
         <img src="/NavLogo.png" alt="Nexura Logo" />
       </div>
 
+      {/* Hamburger Icon */}
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
+
       {/* Nav Links */}
-      <ul className={styles.navLinks}>
+      <ul className={`${styles.navLinks} ${isOpen ? styles.open : ""}`}>
         <li><a href="/" className={styles.active}>Home</a></li>
         <li><a href="#team">Coordinators</a></li>
         <li><a href="#events">Events</a></li>
